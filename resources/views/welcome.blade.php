83e1ee318fe3d0b00cf643386abcbd22476b3aaf
= <=>
2019-11-12 19:27:36 +0700

    Update code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravelaaaaaaaaaa
+                    Laravel
                 </div>
 
                 <div class="links">
